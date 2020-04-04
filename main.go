d9188ab7fd2d5635e197b9bcfaa4c4b840237cab
Yuri <yuri@appsflyer.com>
2020-04-04 15:26:37 +0300

    add base file

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"strconv"
+	"strings"
 
 	"database/sql"
 
@@ -13,6 +15,7 @@ import (
 )
 
 var (
+	baseFilePath  = flag.String("base", "", "the base file csv path")
 	mainFilePath  = flag.String("main", "", "the main file csv path")
 	cybusFilePath = flag.String("cybus", "", "the cybus csv")
 	outFilePath   = flag.String("out", "out.xlsx", "the output file (default: out.xlsx)")
@@ -23,8 +26,8 @@ func init() {
 	if *mainFilePath == "" {
 		panic("must provide the main file path")
 	}
-	if *cybusFilePath == "" {
-		panic("must provide the cibus file path")
+	if *baseFilePath == "" {
+		panic("must provide the base file path")
 	}
 
 }
@@ -39,8 +42,13 @@ func parseMain(path string, db *sql.DB) error {
 		fmt.Print("Total Lines ", sheet1.MaxRow, sheet1.Name)
 		for i := 1; i <= (int(sheet1.MaxRow)); i++ {
 			row1 := sheet1.Row(i)
-			q := fmt.Sprintf(`INSERT INTO main (account, enter_noon, to_date, from_date, evning, noon, morning, amount, budget_num, recepie_id, name, budget_name, departament) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s");`,
-				row1.Col(12), row1.Col(11), row1.Col(10), row1.Col(9), row1.Col(8), row1.Col(7), row1.Col(6), row1.Col(5), row1.Col(4), row1.Col(3), row1.Col(2), row1.Col(1), row1.Col(0))
+			var name string
+			s := strings.Split(row1.Col(2), " ")
+			name = strings.Join(strings.Split(row1.Col(2), " "), " ") //fix name encoding hebrew
+			q := fmt.Sprintf(`INSERT INTO main (account, enter_noon, to_date, from_date, evning, noon, morning, amount, budget_num, recepie_id, name, budget_name, departament, first_name, last_name) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s");`,
+				row1.Col(12), row1.Col(11), row1.Col(10), row1.Col(9), row1.Col(8), row1.Col(7), row1.Col(6), row1.Col(5), row1.Col(4), row1.Col(3), name, row1.Col(1), row1.Col(0),
+				s[0],                     //first name
+				strings.Join(s[1:], " ")) //last name
 			if _, err := db.Exec(q); err != nil {
 				log.Println("insert main error:", err)
 				continue
@@ -48,6 +56,28 @@ func parseMain(path string, db *sql.DB) error {
 			log.Println("exec query:", q)
 		}
 	}
+	return nil
+}
+func parseBase(path string, db *sql.DB) error {
+	log.Println("the base file path:", path)
+	f, err := excelize.OpenFile(path)
+	if err != nil {
+		return err
+	}
+	// Get all the rows in the Sheet1.
+	rows := f.GetRows("Sheet1")
+	for _, row := range rows {
+		// code TEXT, site_code TEXT, site TEXT, employe_id TEXT, kibutz_id TEXT, launch_site_id TEXT, price TEXT, first_name TEXT, last_name TEXT
+		q := fmt.Sprintf(`INSERT INTO base (code, site_code, site, employe_id, kibutz_id, launch_site_id, price, first_name, last_name) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s");`,
+			row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])
+		if _, err := db.Exec(q); err != nil {
+			log.Println("insert cybus error:", err)
+			continue
+		}
+		log.Println("exec query:", q)
+
+	}
+
 	return nil
 }
 
@@ -64,8 +94,11 @@ func parseCybus(path string, db *sql.DB) error {
 			continue
 		}
 		// group_number TEXT, company TEXT, group_name TEXT, departament TEXT, full_name TEXT, employe_id TEXT, total_full TEXTS, total TEXT, company_part TEXT, company_part_amount TEXT, employe_part TEXT, employe_part_amount TEXT, deal_amount TEXT, deal_amount_copy TEXT, first_name TEXT, last_name TEXT, id TEXT
+		var name string
+		s := strings.Split(row[4], " ")
+		name = strings.Join(s[1:], " ") + " " + s[0] //fix name order
 		q := fmt.Sprintf(`INSERT INTO cybus (group_number, company, group_name, departament, full_name, employe_id, total_full, total, company_part, company_part_amount, employe_part, employe_part_amount, deal_amount, deal_amount_copy, first_name, last_name, id) VALUES ("%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s", "%s");`,
-			row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16])
+			row[0], row[1], row[2], row[3], name, row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16])
 		if _, err := db.Exec(q); err != nil {
 			log.Println("insert cybus error:", err)
 			continue
@@ -78,7 +111,8 @@ func parseCybus(path string, db *sql.DB) error {
 
 func initDBSQL(db *sql.DB) error {
 	batch := []string{
-		`CREATE TABLE main ( account TEXT, enter_noon TEXT, to_date TEXT, from_date TEXT, evning TEXT, noon TEXT, morning TEXT, amount TEXT, budget_num TEXT, recepie_id TEXT, name TEXT, budget_name TEXT, departament TEXT);`,
+		`CREATE TABLE main ( account TEXT, enter_noon TEXT, to_date TEXT, from_date TEXT, evning TEXT, noon TEXT, morning TEXT, amount TEXT, budget_num TEXT, recepie_id TEXT, name TEXT, budget_name TEXT, departament TEXT, first_name TEXT, last_name TEXT);`,
+		`CREATE TABLE base ( code TEXT, site_code TEXT, site TEXT, employe_id TEXT, kibutz_id TEXT, launch_site_id TEXT, price TEXT, first_name TEXT, last_name TEXT);`,
 		`CREATE TABLE cybus (group_number TEXT, company TEXT, group_name TEXT, departament TEXT, full_name TEXT, employe_id TEXT, total_full TEXTS, total TEXT, company_part TEXT, company_part_amount TEXT, employe_part TEXT, employe_part_amount TEXT, deal_amount TEXT, deal_amount_copy TEXT, first_name TEXT, last_name TEXT, id TEXT);`,
 	}
 	for _, b := range batch {
@@ -89,33 +123,68 @@ func initDBSQL(db *sql.DB) error {
 	return nil
 }
 
-func createSQLTables(mainPath, cybusPath string, db *sql.DB) error {
+func createSQLTables(basePath, mainPath, cybusPath string, db *sql.DB) error {
 	if err := initDBSQL(db); err != nil {
 		return err
 	}
+
+	if err := parseBase(basePath, db); err != nil {
+		return err
+	}
+
 	if err := parseMain(mainPath, db); err != nil {
 		return err
 	}
 
-	if err := parseCybus(cybusPath, db); err != nil {
-		return err
-	}
+	// if err := parseCybus(cybusPath, db); err != nil {
+	// 	return err
+	// }
 	return nil
 }
 
 func cretaeOutpuFile(path string, db *sql.DB) error {
 	log.Println("create output file", path)
-	hedaer := map[string]string{"A1": "Small", "B1": "Normal", "C1": "Large", "D1": "Apple", "E1": "Orange", "F1": "Pear"}
+	hedaer := map[string]string{"A1": "employee_id", "B1": "full_name", "C1": "launch_price", "D1": "lunch_amount", "E1": "company_charge", "F1": "employe_charge", "G1": "lunch_total"}
 	// values := map[string]int{"B2": 2, "C2": 3, "D2": 3, "B3": 5, "C3": 2, "D3": 4, "B4": 6, "C4": 7, "D4": 8}
 	f := excelize.NewFile()
 	for k, v := range hedaer {
 		f.SetCellValue("Sheet1", k, v)
 	}
-	// for k, v := range values {
-	// 	f.SetCellValue("Sheet1", k, v)
-	// }
+	rows, err := db.Query(`SELECT main.recepie_id, main.name, main.amount, main.noon FROM main JOIN `)
+	if err != nil {
+		return err
+	}
 
-	// Save xlsx file by the given path.
+	i := 2
+	for rows.Next() {
+		var recepie_id, name, amount, noon string
+		if err := rows.Scan(&recepie_id, &name, &amount, &noon); err != nil {
+			log.Println("Get an error when scan row, error:", err)
+			continue
+		}
+		f.SetCellValue("Sheet1", fmt.Sprintf("A%d", i), recepie_id)
+		f.SetCellValue("Sheet1", fmt.Sprintf("B%d", i), name)
+		f.SetCellValue("Sheet1", fmt.Sprintf("C%d", i), "24")
+		f.SetCellValue("Sheet1", fmt.Sprintf("D%d", i), noon)
+		noonInt, err := strconv.ParseInt(noon, 10, 64)
+		if err != nil {
+			log.Println("failed to parse noon lunches nuber, error", err)
+		}
+		f.SetCellValue("Sheet1", fmt.Sprintf("E%d", i), noonInt*24)
+		totalCharge, err := strconv.ParseFloat(amount, 64)
+		if err != nil {
+			log.Println("failed to parse flot total charge,", err)
+		}
+		c := totalCharge - float64(noonInt*24)
+		if c > 0 {
+			f.SetCellValue("Sheet1", fmt.Sprintf("F%d", i), fmt.Sprintf("%.2f", c))
+		} else {
+			f.SetCellValue("Sheet1", fmt.Sprintf("F%d", i), "")
+		}
+
+		f.SetCellValue("Sheet1", fmt.Sprintf("G%d", i), amount)
+		i++
+	}
 	return f.SaveAs(path)
 }
 
@@ -126,37 +195,11 @@ func main() {
 	}
 	defer db.Close()
 
-	if err := createSQLTables(*mainFilePath, *cybusFilePath, db); err != nil {
+	if err := createSQLTables(*baseFilePath, *mainFilePath, *cybusFilePath, db); err != nil {
 		log.Panic(err)
 	}
 
 	if err := cretaeOutpuFile(*outFilePath, db); err != nil {
 		log.Panic(err)
 	}
-
-	// rows, err := db.Query(`SELECT * FROM main`)
-	// if err != nil {
-	// 	log.Println("get wrror when run query, ", err)
-	// }
-
-	// for rows.Next() {
-	// 	var s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13 string
-	// 	if err := rows.Scan(&s1, &s2, &s3, &s4, &s5, &s6, &s7, &s8, &s9, &s10, &s11, &s12, &s13); err != nil {
-	// 		log.Println("Get an error when scan row, error:", err)
-	// 	}
-	// 	log.Println("the main query row:", s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13)
-	// }
-
-	// rows, err = db.Query(`SELECT * FROM cybus`)
-	// if err != nil {
-	// 	log.Println("get wrror when run query, ", err)
-	// }
-
-	// for rows.Next() {
-	// 	var s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17 string
-	// 	if err := rows.Scan(&s1, &s2, &s3, &s4, &s5, &s6, &s7, &s8, &s9, &s10, &s11, &s12, &s13, &s14, &s15, &s16, &s17); err != nil {
-	// 		log.Println("Get an error when scan row, error:", err)
-	// 	}
-	// 	log.Println("the cybus query row:", s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13, s14, s15, s16, s17)
-	// }
 }
